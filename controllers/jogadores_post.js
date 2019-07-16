5ff9ac9e4a54d96c0747a9955740635a0ca68c6c
EdsonFragnan <edsonl.fragnan@gmail.com>
2019-07-16 15:34:18 -0300

    Update jogadores_post.js

--- controllers/jogadores_post.js
+++ controllers/jogadores_post.js
@@ -1,6 +1,10 @@
 'use strict';
 
-/* POST - Grava todos os jogadores */
+/* POST - Grava todos os jogadores 
+    nome_jogador: String
+    gols: number
+    assistencias: number
+*/
 module.exports.jogadores = (req, res) => {
     res.json('Jogadores');
   };
